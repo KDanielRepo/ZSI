f1e2a2c132d76953d226f08283917d76ab096ce0
Komputer <Daniel200825@wp.pl>
2019-10-24 11:31:41 +0200

    k

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,17 +8,16 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Game extends Application{
-    //Integer[][] a = new Integer[][]{{2,4,8,16},{32,64,128,256},{512,1028,2056,2},{4,8,16,32}};
     Integer[][] a = new Integer[4][4];
     Integer randomA;
     Integer randomB;
     Boolean up, right, down, left;
     GridPane gridPane;
+    Scene scene;
+    TextArea score;
 
     public void setA() {
         for (int i = 0; i < 4; i++) {
@@ -29,13 +28,39 @@ public class Game extends Application{
     }
 
     public void random() {
+        int test = 0;
         randomA = ThreadLocalRandom.current().nextInt(0, 4);
         randomB = ThreadLocalRandom.current().nextInt(0, 4);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if(a[i][j]>0){
+                    test++;
+                }
+            }
+        }
+        if(test==16){
+            System.out.println("nie");
+            return;
+        }
+        if(a[randomA][randomB]!=0){
+            System.out.println("tez nie");
+            random();
+        }
+        a[randomA][randomB] = 2;
+    }
+    public void calculateScore(){
+        int scoree = 0;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                scoree+=a[i][j];
+            }
+        }
+        score.setText(Integer.toString(scoree));
     }
 
     public void start(Stage primaryStage) throws Exception {
         BorderPane borderPane = new BorderPane();
-        borderPane.setPrefSize(800, 800);
+        borderPane.setPrefSize(1000, 800);
         VBox vBox = new VBox();
         gridPane = new GridPane();
         gridPane.setPrefSize(600, 600);
@@ -45,23 +70,15 @@ public class Game extends Application{
         left = false;
 
         setA();
-        random();
+        randomA = ThreadLocalRandom.current().nextInt(0, 4);
+        randomB = ThreadLocalRandom.current().nextInt(0, 4);
         a[randomA][randomB] = 2;
         paint(gridPane);
-        /*while (!up && !right && !down && !left) {
 
-        }*/
-
-        Button run = new Button();
-        vBox.getChildren().add(run);
-        borderPane.setCenter(gridPane);
-        borderPane.setRight(vBox);
-
-        Scene scene = new Scene(borderPane);
+        scene = new Scene(borderPane);
         scene.setOnKeyPressed(new EventHandler<javafx.scene.input.KeyEvent>() {
             @Override
             public void handle(javafx.scene.input.KeyEvent event) {
-                System.out.println(event.getCode());
                 switch (event.getCode()){
                     case UP:
                         up = true;
@@ -77,12 +94,12 @@ public class Game extends Application{
                         break;
                 }
                 update();
+                scene.getRoot().requestFocus();
             }
         });
-        /*scene.setOnKeyReleased(new EventHandler<javafx.scene.input.KeyEvent>() {
+        scene.setOnKeyReleased(new EventHandler<javafx.scene.input.KeyEvent>() {
             @Override
             public void handle(javafx.scene.input.KeyEvent event) {
-                System.out.println(event.getCode());
                 switch (event.getCode()){
                     case UP:
                         up = false;
@@ -98,12 +115,20 @@ public class Game extends Application{
                         break;
                 }
                 update();
+                scene.getRoot().requestFocus();
             }
-        });*/
+        });
+
+        score = new TextArea();
+        Button run = new Button();
+        vBox.getChildren().add(run);
+        vBox.getChildren().add(score);
+        borderPane.setCenter(gridPane);
+        borderPane.setRight(vBox);
+
         primaryStage.setTitle("ZSI");
         primaryStage.setScene(scene);
         primaryStage.show();
-
     }
     public void paint(GridPane gridPane){
         for (int i = 0; i < 4; i++) {
@@ -111,65 +136,102 @@ public class Game extends Application{
                 TextArea test = new TextArea();
                 test.setPrefSize(150, 150);
                 test.setEditable(false);
-                test.setText(Integer.toString(a[i][j]));
-                gridPane.add(test, j, i);
+                if(a[i][j]==0){
+                    test.setText("");
+                }else {
+                    test.setText(Integer.toString(a[i][j]));
+                }
+                gridPane.add(test, i, j);
             }
         }
     }
     public void update(){
-        if(up){
-            if(a[0][0]==0&&a[1][0]==0&&a[2][0]==0&&a[3][0]==0){
-                for(int i = 0; i<4;i++){
-                    for(int j = 0; j<3;j++){
-                        a[i][j]=a[i][j+1];
-                    }
-                }
-            }
-            paint(gridPane);
-            printA();
-        }
-        if(right){
-            if(a[3][0]==0&&a[3][1]==0&&a[3][2]==0&&a[3][3]==0){
-                for(int i = 3; i>0;i--){
-                    for(int j = 0;j<4;j++){
-                        a[i][j]=a[i-1][j];
-                    }
-                }
-            }
-            paint(gridPane);
-            printA();
-        }
-        if(down){
-            if(a[0][3]==0&&a[1][3]==0&&a[2][3]==0&&a[3][3]==0){
-                for(int i =0;i<4;i++){
-                    for(int j = 3; j>0;j--){
-                        a[i][j]=a[i][j-1];
-                    }
-                }
-            }
-            paint(gridPane);
-            printA();
-        }
         if(left){
-            if(a[0][0]==0&&a[0][1]==0&&a[0][2]==0&&a[0][3]==0){
+            for(int k = 0; k<4;k++) {
                 for (int i = 0; i < 3; i++) {
                     for (int j = 0; j < 4; j++) {
-                        a[i][j]=a[i+1][j];
+                        if(a[i][j].equals(a[i+1][j])){
+                            a[i][j]=a[i][j]*2;
+                            a[i+1][j]=0;
+                        }
+                        if (a[i][j] == 0) {
+                            if (a[i + 1][j] != 0) {
+                                a[i][j] = a[i + 1][j];
+                                a[i + 1][j] = 0;
+                            }
+                        }
                     }
                 }
             }
+            random();
             paint(gridPane);
-            printA();
+            calculateScore();
         }
-    }
-    public void printA(){
-        for(int i = 0;i<4;i++){
-            System.out.println("\n");
-            for (int j = 0; j <4 ; j++) {
-                System.out.println(a[j][i]);
+        if(right){
+            for(int k = 0; k<4;k++) {
+                for (int i = 3; i > 0; i--) {
+                    for (int j = 0; j < 4; j++) {
+                        if(a[i][j].equals(a[i-1][j])){
+                            a[i][j]=a[i][j]*2;
+                            a[i-1][j]=0;
+                        }
+                        if (a[i][j] == 0) {
+                            if (a[i - 1][j] != 0) {
+                                a[i][j] = a[i - 1][j];
+                                a[i - 1][j] = 0;
+                            }
+                        }
+                    }
+                }
             }
+            random();
+            paint(gridPane);
+            calculateScore();
+        }
+        if(up){
+            for(int k = 0; k<4;k++) {
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if(a[i][j].equals(a[i][j+1])){
+                            a[i][j]=a[i][j]*2;
+                            a[i][j+1]=0;
+                        }
+                        if (a[i][j] == 0) {
+                            if (a[i][j + 1] != 0) {
+                                a[i][j] = a[i][j + 1];
+                                a[i][j + 1] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            random();
+            paint(gridPane);
+            calculateScore();
+        }
+        if(down){
+            for(int k = 0; k<4;k++) {
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 3; j > 0; j--) {
+                        if(a[i][j].equals(a[i][j-1])){
+                            a[i][j]=a[i][j]*2;
+                            a[i][j-1]=0;
+                        }
+                        if (a[i][j] == 0) {
+                            if (a[i][j - 1] != 0) {
+                                a[i][j] = a[i][j - 1];
+                                a[i][j - 1] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+            random();
+            paint(gridPane);
+            calculateScore();
         }
     }
+
     public static void main(String[] args) {
         launch(Game.class, args);
     }
